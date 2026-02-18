2ae005dbaacb53951926385a132d43a0636aea1b
Syed <xayanmay@gmail.com>
2026-02-18 13:00:18 -0500

    created allCharacterList

--- src/dom-helpers.js
+++ src/dom-helpers.js
@@ -1,14 +1,30 @@
-const detailsContainer = document.querySelector('#character-details-container');
+const detailsContainer = document.querySelector('#character-details');
 
 export const renderCharacterDetails = (character) => {
-    const detailsSection = document.querySelector('#character-details');
-    detailsSection.classList.remove('hidden');
+    detailsContainer.classList.remove('hidden');
 
     detailsContainer.innerHTML = `
-    <h3>${character.name}</h3>
-    <p><strong>Birth Year:</strong> ${character.birth_year}</p>
-    <p><strong>Height:</strong> ${character.height}</p>
-    <p><strong>Mass:</strong> ${character.mass}</p>
-    <p><strong>Gender:</strong> ${character.gender}</p>
-  `;
+        <h3>${character.name}</h3>
+        <p><strong>Birth Year:</strong> ${character.birth_year}</p>
+        <p><strong>Height:</strong> ${character.height}</p>
+        <p><strong>Mass:</strong> ${character.mass}</p>
+        <p><strong>Gender:</strong> ${character.gender}</p>
+    `;
+};
+
+const characterList = document.getElementById('characters-list');
+
+export const renderCharacterList = (characters) => {
+    characterList.innerHTML = ''; 
+
+    characters.forEach(character => {
+        const li = document.createElement('li'); 
+        li.textContent = character.name;
+
+        li.addEventListener('click', () => {
+            renderCharacterDetails(character);
+        });
+
+        characterList.appendChild(li);
+    });
 };
