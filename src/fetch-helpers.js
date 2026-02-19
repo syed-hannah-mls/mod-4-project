f2ddbd7d41975d6b2e0f3a02fd024bd9c2b97ba0
Syed <xayanmay@gmail.com>
2026-02-19 14:29:54 -0500

    Changes in design

--- src/fetch-helpers.js
+++ src/fetch-helpers.js
@@ -1,16 +1,15 @@
-export const getCharacter = async () => {
-    try {
-        const response = await fetch(`https://swapi.dev/api/people/`)
-        if(!response.ok){
+export const getAllCharacters = async () => {
+    let allCharacters = [];
+    let url = 'https://swapi.dev/api/people/';
 
-            throw new Error('Error occurred')
-        }
-            const data = await response.json()
-            return { data: data, error: null}
-    } 
-    catch (error) {
-        console.warn(error)
-        return { data: null, error: error.message }
+    while (url) {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Failed to fetch characters');
+
+        const data = await response.json();
+        allCharacters = allCharacters.concat(data.results);
+        url = data.next; 
     }
-};
 
+    return allCharacters;
+};
