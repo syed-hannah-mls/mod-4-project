034dd354e0814dd3e4434906147dbfa5c5059f19
Syed <xayanmay@gmail.com>
2026-02-19 15:13:23 -0500

    debug fetch helpers

--- src/fetch-helpers.js
+++ src/fetch-helpers.js
@@ -1,25 +1,23 @@
 export const getAllCharacters = async () => {
-    let allCharacters = [];
-    let url = 'https://swapi.dev/api/people/';
+  let allCharacters = [];
+  let url = "https://swapi.dev/api/people/";
 
-    while (url) { //This loop is to keep making fetch calls to recieve more than 10 people
-        const response = await fetch(url);
-        if (!response.ok) throw new Error('Failed to fetch characters');
+  while (url) {
+    //This loop is to keep making fetch calls to recieve more than 10 people
+    try {
+      const response = await fetch(url);
+      if (!response.ok) throw new Error("Failed to fetch characters");
+      
+      const data = await response.json();
 
-        const data = await response.json();
-        allCharacters = allCharacters.concat(data.results);
-        url = data.next; //at some point this will make the url falsy
-            throw new Error('Error occurred')
-        }
-            const data = await response.json()
-            return { data: data, error: null}
+      allCharacters = allCharacters.concat(data.results); 
+      url = data.next; //at some point this will make the url falsy
+
+    } catch (error) {
+      console.warn(error);
+      return { data: null, error: error.message };
     }
-    
-    //resolved
-    catch (error) {
-        console.warn(error)
-        return { data: null, error: error.message }
-    }
+  }
 
-    return allCharacters;
+  return allCharacters;
 };
